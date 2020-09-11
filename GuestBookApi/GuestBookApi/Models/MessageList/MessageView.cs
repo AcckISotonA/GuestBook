74b0de6d20814912544e7d9893aae123654a4684
Mikhail Bocharov <bgmihail@list.ru>
2020-09-11 18:10:40 +1000

    ++

---
+++ GuestBookApi/GuestBookApi/Models/MessageList/MessageView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GuestBookApi.Models.MessageList
+{
+    public class MessageView
+    {
+        public DateTime DateC { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Text { get; set; }
+    }
+}
